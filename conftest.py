a34aba07bd6ebbc9df4200f67e0aaa9587f15408
Raphaël Pfister <rapfister@airfrance.fr>
2024-01-24 17:15:55 +0000

    tests: adding giskard fixtures

--- conftest.py
+++ conftest.py
@@ -5,11 +5,25 @@ import pytest
 import pickle
 
 import pandas as pd
+from giskard import Dataset, transformation_function, slicing_function
+from delay_predictor.trainer import CATEGORICAL, NUMERICAL, TARGET
 
 
 @pytest.fixture(scope="function")
 def df():
-    return pd.read_parquet("./train_data.parquet")
+    return pd.read_parquet("./df_test.parquet")
+
+
+@pytest.fixture(scope="function")
+def giskard_dataset():
+    df = pd.read_parquet("./df_test.parquet")
+    giskard_dataset = Dataset(
+        df=df,
+        target="departure_delay",
+        name="flight_delays",
+        cat_columns=CATEGORICAL
+    )
+    return giskard_dataset
 
 
 @pytest.fixture(scope="module")
@@ -22,3 +36,17 @@ def encoder():
 def model():
     with open("model.pickle", "rb") as f:
         return pickle.load(f)
+
+
+@pytest.fixture(scope="module")
+@transformation_function(name="increase previous delay")
+def increase_previous_delay(row):
+    row["previous_arrival_delay"] = row["previous_arrival_delay"] + 5.
+    return row
+
+
+@pytest.fixture(scope="module")
+@transformation_function(name="increase previous delay")
+def decrease_previous_delay(row):
+    row["previous_arrival_delay"] = row["previous_arrival_delay"] / 1.2
+    return row
